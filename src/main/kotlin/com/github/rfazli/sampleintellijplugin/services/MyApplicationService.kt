97e0e4385b18742a3897ce5bfd2ba43527fc08cd
GitHub Action <action@github.com>
2021-01-19 20:03:27 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/rfazli/sampleintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rfazli.sampleintellijplugin.services
+
+import com.github.rfazli.sampleintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
